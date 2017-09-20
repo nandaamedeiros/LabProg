a6fb283fceac79d34b32766d858ec146828a2879
Fernanda Medeiros <nandinhamedeiros38@gmail.com>
2017-09-19 23:57:27 -0300

    lab01

--- avaliacoes/Lab01/src/programa/MinMax.java
+++ avaliacoes/Lab01/src/programa/MinMax.java
@@ -1,5 +1,36 @@
 package programa;
 
+import java.util.Scanner;
+
+/*
+ * Aluno 01: Ivanildo Simplício da Silva Filho
+ * Aluno 02: Fernanda Eduarda de Medeiros Silva
+ * Aluno 03: Wesley Porto Santos
+ */
+
 public class MinMax {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int valores[] = new int[3];
+		int min, max;
+		
+		for(int i = 0; i < valores.length; i++) {
+			System.out.printf("Insira o valor %d: ", i+1);
+			valores[i] = sc.nextInt();
+		}
+		
+		min = max = valores[0];
+		for(int i = 1; i < valores.length; i++) {
+			if(valores[i] > max) {
+				max = valores[i];
+			}else if(valores[i] < min){
+				min = valores[i];
+			}
+		}
+		
+		System.out.println("Menor valor: "+min);
+		System.out.println("Maior valor: "+max);
+	}
 }
+
